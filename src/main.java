618c3a2a8f10ea7c13e2325bc9a6d971dd6d3167
sathiya <sathiyanarayananr67@gmail.com>
2026-08-19 23:12:26 +0530

    myfirstjavaprogramming

---
+++ src/main.java
@@ -0,0 +1,21 @@
+public class main {
+
+    public static void main(String[] args){
+
+        // This is my first java program
+
+        /*
+        This
+        is
+        a
+        multi
+        line
+        comment
+         */
+
+        System.out.println("hello world");
+        System.out.println("It's really very bad");
+        System.out.println("Buy me world ");
+
+    }
+}
