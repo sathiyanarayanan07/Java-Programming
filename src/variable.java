a74f222e2321c6e55ee6c2464d65a5f418db3620
sathiya <sathiyanarayananr67@gmail.com>
2026-08-19 23:48:18 +0530

    myfirstvariable

---
+++ src/variable.java
@@ -0,0 +1,63 @@
+public class variable {
+
+    public static void main(String[] args) {
+
+        // 1.declaration
+
+        int age = 23;
+        int year = 2026;
+        int quantity = 1;
+
+        System.out.println("The year is " + year);
+
+        // double
+
+        double price = 19.99;
+        double gpa = 3.5;
+        double temp = 12.5;
+
+        System.out.println("$" + price);
+
+        // char
+
+        char grade = 'A';
+        char symbol = 'L';
+        char current = '$';
+
+        System.out.println(current);
+
+        // boolean
+
+        boolean isStudent = false;
+        boolean forSale = false;
+        boolean isOnline = true;
+
+
+        if(isStudent){
+            System.out.println("You are a student");
+        }
+        else{
+            System.out.println("You are not a student");
+        }
+
+
+
+        // Reference
+
+        // string
+
+        String name = "sathiya";
+        String food = "Pizza";
+        String email  = "fake123@gmail.com";
+
+        System.out.println("Hello " + name);
+        System.out.println("You are " + age + " years old");
+        System.out.println("fav food: " + food);
+        System.out.println("email: " + email);
+
+
+    }
+
+
+
+}
